f8a0035f9159b7e3baf917e1bb1dbc81415740c9
Al1exhawk <aldevid9@gmail.com>
2019-03-19 17:49:37 +0200

    Updates

---
+++ precache-manifest.89aa8ea9a8c706865b8c113a195cec5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c2e27197b3338a18e7b",
+    "url": "/ContactManager/static/js/runtime~main.8c2e2719.js"
+  },
+  {
+    "revision": "7df44edd877146308eb7",
+    "url": "/ContactManager/static/js/main.7df44edd.chunk.js"
+  },
+  {
+    "revision": "a7742aa9e69a35aa1904",
+    "url": "/ContactManager/static/js/2.a7742aa9.chunk.js"
+  },
+  {
+    "revision": "a7742aa9e69a35aa1904",
+    "url": "/ContactManager/static/css/2.e533585b.chunk.css"
+  },
+  {
+    "revision": "16170156f2c81cbea4106a4dc3cc96db",
+    "url": "/ContactManager/index.html"
+  }
+];
